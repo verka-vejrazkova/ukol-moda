f6b1172d2b145bcfd653c5f3ad78e4f7050902bc
verka-vejrazkova <71260911+verka-vejrazkova@users.noreply.github.com>
2022-03-17 21:15:55 +0100

    final

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -6,12 +6,14 @@ const Footer = () => {
 
     
     const year = "2022";
-    const author = "Czechitas"
+    const author = "Czechitas";
 
     return (
         <>
+        <footer className="footer">
     <p className="footer__copyright">&copy; 2022, Czechitas</p>
     <p className="footer__disclosure">Jsme fiktivní firma, která nic neprodává a v žádném případě ani za nic neručí.</p>
+        </footer>
     </>
     )}
 
